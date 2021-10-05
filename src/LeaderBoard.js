5b263aeaaa4d5ed9af058c4cfbd3a06292d65953
arifbd2221 <mohidulhoque216@gmail.com>
2021-10-05 09:41:32 +0600

    handled only banglalink phone number and added name field on registration

--- src/LeaderBoard.js
+++ src/LeaderBoard.js
@@ -7,26 +7,52 @@ import { useState } from 'react';
 import { Table, Alert, Button } from 'react-bootstrap';
 import './App.css';
 import { useHistory } from 'react-router-dom';
-
+import Loader from "react-loader-spinner";
+import React from "react";
 
 
 export const LeaderBoard = () => {
     const history = useHistory ();
-    const [players, setPlayers] = useState()
+    const [players, setPlayers] = useState();
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState();
 
     useEffect(() => {
-        axios.get("https://gamee-backend.herokuapp.com/api/v2/leaderboard/list")
+        axios.get("http://localhost:8000/api/v2/leaderboard/list")
         .then(resp => {
             setPlayers(resp.data)
+            setLoading(false);
         })
+        .catch(err => {
+            console.log(err);
+            setLoading(false);
+        })
+        
+        if(localStorage){
+            const token = localStorage.getItem("token");
+            const headers = {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`
+            }
+            axios.get("http://localhost:8000/api/v2/account/user", {headers: headers})
+            .then(resp => {
+                console.log(resp.data);
+                setUser(resp.data);
+            })
+            .catch(error => {
+                console.log("Score push error", error);
+            })
+        }
+        return
     }, [])
 
 
     console.log(players);
 
     return (
+        <React.Fragment>
         <div className="leaderboard">
-            <Alert variant="primary">
+            <Alert variant="primary" style={{backgroundColor: "#ff8126", color: "white", fontWeight: "bold"}}>
                 Leaderboard
             </Alert>
             <div className="tableStyle">
@@ -34,27 +60,35 @@ export const LeaderBoard = () => {
             <thead>
                 <tr>
                 <th>Rank</th>
-                <th>Phone</th>
+                <th>Name</th>
                 <th>Score</th>
                 <th>Game</th>
                 </tr>
             </thead>
             <tbody>
                 {players?.map((player) => (   
-                        <tr>
+                        <tr style={player?.phone === user?.phone? {color: "#ff8126"}: {}}>
                             <td>{player.rank}</td>
-                            <td>{player.phone}</td>
+                            <td>{player.fullname}</td>
                             <td>{player.score}</td>
                             <td>{player.game}</td>
                         </tr>
                         )) }
-
             </tbody>
             </Table>
             </div>
-            <div className="play">
-            <Button variant="primary" onClick={e => history.push("/play")}>Play Again</Button>
+                <div className="play">
+                    <Button class="buttonL" variant="primary" onClick={e => history.push("/play")} style={{backgroundColor: "#ff8126", color: "white", borderColor: "#f9fafb", borderRadius: "10px"}}>Play Again</Button>
+                </div>
             </div>
-            </div>
+            <Loader
+                type="Bars"
+                color="#00BFFF"
+                height={100}
+                width={100}
+                visible={loading}
+                className="loader-center"
+            />
+            </React.Fragment>
     )
 }
